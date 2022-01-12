2a0f4569f8b28179670ea87327cd710f5eb57559
oscar120020 <omartinez@intellisys.com.do>
2022-01-12 16:54:19 -0400

    evento para visualizar el numero de tickets pendientes

--- models/sockets.js
+++ models/sockets.js
@@ -14,12 +14,14 @@ class Sockets {
             socket.on("solicitar-ticket", (_, callback) => {
                 const newTicket = this.ticketList.createTicket()
                 callback(newTicket)
+                this.io.emit("tickets-pendientes", this.ticketList.pendientes.length)
             })
 
             socket.on("siguiente-ticket", ({agente, escritorio}, callback) => {
                 const myTicket = this.ticketList.asignarTicket(agente, escritorio)
                 callback(myTicket)
                 this.io.emit("ticket-asignado", this.ticketList.last13)
+                this.io.emit("tickets-pendientes", this.ticketList.pendientes.length)
             })
 
         });
